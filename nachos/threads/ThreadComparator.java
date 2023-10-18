4675954c659b5ec82b06b14c213b0be203415776
agurel33 <agurel@ucsd.edu>
2023-10-18 15:53:50 -0700

    Update ThreadComparator.java

--- nachos/threads/ThreadComparator.java
+++ nachos/threads/ThreadComparator.java
@@ -3,10 +3,11 @@ import java.util.Comparator;
 
 public class ThreadComparator implements Comparator<Pair> {
     public int compare(Pair t1, Pair t2) {
-        if(t1.wakeTime > t2.wakeTime) {
-            
+        if(t1.getWakeTime() > t2.getWakeTime()) {
+            return 1;
         }
-
-        return int;
+        else {
+            return -1;
+        }
     }
 }
