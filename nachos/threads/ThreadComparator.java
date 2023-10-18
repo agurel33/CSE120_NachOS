178a1acf02936bbbf9571e7265e8e3e2ae579ea3
agurel33 <agurel@ucsd.edu>
2023-10-18 15:51:21 -0700

    Create ThreadComparator.java

---
+++ nachos/threads/ThreadComparator.java
@@ -0,0 +1,12 @@
+package nachos.threads;
+import java.util.Comparator;
+
+public class ThreadComparator implements Comparator<Pair> {
+    public int compare(Pair t1, Pair t2) {
+        if(t1.wakeTime > t2.wakeTime) {
+            
+        }
+
+        return int;
+    }
+}
