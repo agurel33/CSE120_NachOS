3f6a53c3e4602e1b8550e28f3dcacc9b7742c311
agurel33 <agurel@ucsd.edu>
2023-10-25 16:59:58 -0700

    rende

--- nachos/threads/ThreadedKernel.java
+++ nachos/threads/ThreadedKernel.java
@@ -51,8 +51,8 @@ public class ThreadedKernel extends Kernel {
 		SynchList.selfTest();
 		//Alarm.selfTest();
 		//Condition.selfTest();
-		Condition2.selfTest();
-		//Rendezvous.selfTest();
+		//Condition2.selfTest();
+		Rendezvous.selfTest();
 		if (Machine.bank() != null) {
 			ElevatorBank.selfTest();
 		}
