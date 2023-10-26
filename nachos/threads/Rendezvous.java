96f832d060512284be45c645a55fd272468f199b
agurel33 <agurel@ucsd.edu>
2023-10-25 17:00:56 -0700

    Update Rendezvous.java

--- nachos/threads/Rendezvous.java
+++ nachos/threads/Rendezvous.java
@@ -171,6 +171,7 @@ public class Rendezvous {
 
     public static void selfTest() {
     // place calls to your Rendezvous tests that you implement here
-        rendezTest1();
+        //rendezTest1();
+        ourRendeTest1();
     }
 }
