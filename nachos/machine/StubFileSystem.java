2e628939397b3a62582c667e503c006bd8362a8a
agurel33 <agurel@ucsd.edu>
2023-11-13 14:06:34 -0800

    Update StubFileSystem.java

--- nachos/machine/StubFileSystem.java
+++ nachos/machine/StubFileSystem.java
@@ -26,19 +26,15 @@ public class StubFileSystem implements FileSystem {
 	}
 
 	public OpenFile open(String name, boolean truncate) {
-		if (!checkName(name)) {
-			System.out.println("checkpoint 1: " + name);
+		if (!checkName(name)) 
 			return null;
-		}
 
 		delay();
 
 		try {
-			System.out.println("checkpoint 2");
 			return new StubOpenFile(name, truncate);
 		}
 		catch (IOException e) {
-			System.out.println("checkpoint 3");
 			return null;
 		}
 	}
