6abba46d1e7b324c6933bf1bd31b5e0a1bbc14f8
agurel33 <agurel@ucsd.edu>
2023-11-16 09:02:34 -0800

    Update UserProcess.java

--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -364,7 +364,9 @@ public class UserProcess {
 
 			for (int i = 0; i < section.getLength(); i++) {
 				int vpn = section.getFirstVPN() + i;
+				Lib.debug(dbgProcess, "Entering page #: " + counter);
 				pageTable[counter] = new TranslationEntry(vpn, UserKernel.getNextOpenPage(), true, section.isReadOnly(), false, false);
+				Lib.debug(dbgProcess, "Curr VPN: " + vpn + ", Curr PNP: " + pageTable[vpn].ppn);
 				counter++;
 				// for now, just assume virtual addresses=physical addresses
 				section.loadPage(i, pageTable[i].ppn);
