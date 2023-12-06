8bfe2790a9d8dfd39e8a65694689a117084635ba
agurel33 <agurel@ucsd.edu>
2023-12-06 10:56:38 -0800

    Update VMProcess.java

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -367,6 +367,11 @@ public class VMProcess extends UserProcess {
 			//System.out.println(bad_address + " bad");
 			requestPage(bad_address);
 			return;
+		case Processor.exceptionBusError:
+			Processor proc1 = Machine.processor();
+			int bad_address1 = proc1.getBadAddress();
+			System.out.println(bad_address1);
+			return;
 		default:
 			super.handleException(cause);
 			break;
